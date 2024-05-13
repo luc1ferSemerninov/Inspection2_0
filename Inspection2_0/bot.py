3388d825376cc9ff08d75c05a47960cbf0dc8485
luc1ferSemerninov <semerninov000@gmail.com>
2024-05-13 22:06:47 +0500

    Revert "ред сооб"
    This reverts commit 78f65b7bc038159856e7e75a9acf05cb917a74b4.

--- Inspection2_0/bot.py
+++ Inspection2_0/bot.py
@@ -8,8 +8,6 @@ from .models import Start
 
 all_objects = Start.objects.values()
 
-chat_id = -1002003805171
-
 token = "7156367176:AAHWf4T-36vtV8UjHjDDowYlRY--Myq1OFM"
 webhook_url = "ТВОЙ IP"
 bot = telebot.TeleBot(token)
@@ -24,10 +22,6 @@ for i in all_objects:
     zone = i["zone"]
     department = i["department"]
     H = i["H"]
-    send_photo_button = telebot.types.InlineKeyboardButton("Принять", callback_data=f'claim{H}')
-    keyboard = telebot.types.InlineKeyboardMarkup().add(send_photo_button)
-
-    bot.send_message(chat_id = chat_id, message_thread_id= group, text = f'{department} Вам пришел: {zone}', reply_markup=keyboard)
 
 def set_webhook(request):
     s = bot.set_webhook(webhook_url + '/webhook/')
@@ -47,14 +41,6 @@ def webhook(request):
 def test_message(message):
     bot.reply_to(message, "Привет")
     return 200
-
-@bot.callback_query_handler(func=lambda call: True)
-def handle_callback(call):
-    if call.data.startswith('claim'):
-        H = call.data.replace('claim', '')  # Получаем значение H из callback_data
-        # Отправляем личное сообщение пользователю
-        bot.send_message(call.from_user.id, "Вы приняли обход")
-        bot.edit_message_text(f"@{call.from_user.username} Принял обход", chat_id=chat_id, message_id=call.message.id)
 
 def start_bot():
     t = threading.Thread(target=bot.polling, daemon=True)
